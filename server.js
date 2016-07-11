298a4ac32fa095198f4d8f1a189159bd998f454b
Laurence Hammond <laurence.hammond@bbc.co.uk>
2016-07-11 18:50:36 +0100

    deployment

--- server.js
+++ server.js
@@ -3,21 +3,23 @@ require('babel-register')({
 });
 
 var express = require('express');
+var path = require('path');
 var app = express();
 var React = require('react');
 var ReactDOMServer = require('react-dom/server');
 var Index = require('./src/jsx/pages/Index.jsx');
 
-app.use(express.static('web'));
+module.exports = {
+	app: function() {
+		app.use(express.static(path.join(__dirname, '/web')));
 
-app.get('/', function(req, res) {
-	var html = ReactDOMServer.renderToString(
-		React.createElement(Index)
-	);
-	res.send(html);
-});
+		app.get('/', function(req, res) {
+			var html = ReactDOMServer.renderToString(
+				React.createElement(Index)
+			);
+			res.send(html);
+		});
 
-var PORT = 3000;
-app.listen(PORT, function() {
-	console.log('server running');
-});
+		return app;
+	}
+};
